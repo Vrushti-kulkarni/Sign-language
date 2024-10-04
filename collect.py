1065554a0a8a8420323b1217e8d41e7d166e3ec3
Vrushti-kulkarni <152431838+Vrushti-kulkarni@users.noreply.github.com>
2024-10-04 23:40:28 +0530

    Add files via upload

---
+++ collect.py
@@ -0,0 +1,135 @@
+import cv2
+import os
+import time
+
+#to store all the data in a directory called data
+direct = 'C:/Users/JYOTI/Downloads/DEEP_LEARNING/sign_language/data/train'
+
+#to check if this directory data is already made, if not create new
+if not os.path.exists(direct):
+    os.makedirs(direct)
+
+classes = 3
+dataset_size = 70
+
+#for capturing video, 0 index for default camera, higher index for other camera
+
+
+#now we make a loop such that for every class or label it should collect 100 images
+cap = cv2.VideoCapture(0)
+for j in range(classes):
+    print(j)
+    #basically to check if directory of that class is present in our main directory
+    #join is basically making a directory in base directory
+    if not os.path.exists(os.path.join(direct, str(j))):
+        os.makedirs(os.path.join(direct, str(j)))
+    
+    #print("collecting data for class {}".format(j))
+    cap = cv2.VideoCapture(0)
+    #ROI
+    ret, frame = cap.read()
+    #(x1, y1): Represents the top-left corner of the rectangle.
+    #(x2, y2): Represents the bottom-right corner of the rectangle.
+    x1 = 10
+    y1 = 10
+    x2 = int(0.5 * frame.shape[1])
+    y2 = int(0.5 * frame.shape[0])
+    #frame.shape[0]: The height of the frame (the number of rows, or the number of pixels vertically).
+    #frame.shape[1]: The width of the frame (the number of columns, or the number of pixels horizontally).
+    #frame.shape[2]: The number of channels in the image (typically 3 for RGB images).
+
+    cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (0,255,0), 2)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (0,255,0), 2)
+        cv2.putText(frame, 'get ready!', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 0), 3, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+
+        
+        if cv2.waitKey(10)==ord('q'):
+            break
+    
+    for i in range(0,dataset_size):
+        #to capture image, ret is boolean, and frame stores the image
+        ret, frame = cap.read()
+        cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (0,255,0), 2)
+        #initialise roi
+        roi = frame[y1:y2, x1:x2]
+        roi = cv2.resize(roi, (64,64))
+
+        cv2.imshow('frame',frame)
+
+        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('ROI', roi)
+        cv2.resizeWindow('ROI', 300, 300) 
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(direct, str(j), '{}.jpg'.format(i)), roi)
+        
+     
+cap.release()
+cv2.destroyAllWindows()
+
+#to store all the data in a directory called data
+direct = 'C:/Users/JYOTI/Downloads/DEEP_LEARNING/sign_language/data/test'
+
+#to check if this directory data is already made, if not create new
+if not os.path.exists(direct):
+    os.makedirs(direct)
+
+classes = 3
+dataset_size = 30
+
+cap = cv2.VideoCapture(0)
+for j in range(classes):
+    print(j)
+    #basically to check if directory of that class is present in our main directory
+    #join is basically making a directory in base directory
+    if not os.path.exists(os.path.join(direct, str(j))):
+        os.makedirs(os.path.join(direct, str(j)))
+    
+    #print("collecting data for class {}".format(j))
+    cap = cv2.VideoCapture(0)
+    #ROI
+    ret, frame = cap.read()
+    #(x1, y1): Represents the top-left corner of the rectangle.
+    #(x2, y2): Represents the bottom-right corner of the rectangle.
+    x1 = 10
+    y1 = 10
+    x2 = int(0.5 * frame.shape[1])
+    y2 = int(0.5 * frame.shape[0])
+    #frame.shape[0]: The height of the frame (the number of rows, or the number of pixels vertically).
+    #frame.shape[1]: The width of the frame (the number of columns, or the number of pixels horizontally).
+    #frame.shape[2]: The number of channels in the image (typically 3 for RGB images).
+
+    cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (0,255,0), 2)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (0,255,0), 2)
+        cv2.putText(frame, 'get ready!', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 0), 3, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+
+        
+        if cv2.waitKey(10)==ord('q'):
+            break
+    
+    for i in range(0,dataset_size):
+        #to capture image, ret is boolean, and frame stores the image
+        ret, frame = cap.read()
+        cv2.rectangle(frame, (x1-1, y1-1), (x2+1, y2+1), (0,255,0), 2)
+        #initialise roi
+        roi = frame[y1:y2, x1:x2]
+        roi = cv2.resize(roi, (64,64))
+
+        cv2.imshow('frame',frame)
+
+        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('ROI', roi)
+        cv2.resizeWindow('ROI', 300, 300) 
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(direct, str(j), '{}.jpg'.format(i)), roi)
+        
+     
+cap.release()
+cv2.destroyAllWindows()
---
+++ model_CNN.py
@@ -0,0 +1,62 @@
+import keras
+import pandas as pd
+import numpy as np
+
+from keras import Sequential, layers, models, callbacks, losses, optimizers, metrics
+
+input_size = (64, 64, 1)
+
+model = keras.Sequential(  
+    [
+    keras.layers.Input(shape = input_size),
+    keras.layers.Conv2D(32, kernel_size = (3,3), activation = "relu"),
+    keras.layers.Conv2D(32, kernel_size = (3,3), activation = "relu"),
+    keras.layers.MaxPooling2D(pool_size = (2,2)),
+    keras.layers.Conv2D(16, kernel_size = (3,3), activation = "relu"),
+    keras.layers.Conv2D(16, kernel_size = (3,3), activation = "relu"),
+    keras.layers.GlobalAveragePooling2D(),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation = "relu"),
+    keras.layers.Dense(3, activation = "softmax")
+]
+)
+
+model.summary()
+
+model.compile(
+    loss = keras.losses.CategoricalCrossentropy(),
+    optimizer= keras.optimizers.Adam(), 
+    metrics = [
+                  keras.metrics.SparseCategoricalAccuracy(name = 'categ'), 'accuracy'
+              ]
+)
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training = train_datagen.flow_from_directory('C:/Users/JYOTI/Downloads/DEEP_LEARNING/sign_language/data/train',
+                                                 target_size=(64, 64),
+                                                 batch_size=5,
+                                                 color_mode='grayscale',
+                                                 class_mode='categorical')
+
+testing = test_datagen.flow_from_directory('C:/Users/JYOTI/Downloads/DEEP_LEARNING/sign_language/data/test',
+                                            target_size=(64, 64),
+                                            batch_size=5,
+                                            color_mode='grayscale',
+                                            class_mode='categorical') 
+
+model.fit_generator(
+        training,
+        steps_per_epoch=600, 
+        epochs=10,
+        validation_data=testing,
+        validation_steps=30
+        )
